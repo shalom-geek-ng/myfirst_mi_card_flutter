9c1e5b636d1cfa313fc67c1cbb000db34ee98795
shalom <oluwafemishalom3@gmail.com>
2024-02-29 14:37:24 +0100

    working on my official card project

--- lib/main.dart
+++ lib/main.dart
@@ -15,42 +15,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body:
-        SafeArea(
-          child: Row(
-           // mainAxisSize: MainAxisSize.min,
-            // verticalDirection: VerticalDirection.up,
-            // mainaxis align with parent container
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // follows the largest width crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-            Container(
-            color: Colors.white,
-            width: 100.0,
-            height: double.infinity,
-            child: Text("Container 1"),
-            ),
-
-              SizedBox(
-                width: 30.0,
-              ),
-              Container(
-                width: 100.0,
-                height: 100,
-                color: Colors.blue,
-                child: Text("Container 2"),
-              ),
-              Container(
-                width: 100.0,
-                height: double.infinity,
-                color: Colors.red,
-                child: Text("Container 3"),
-              ),
-
-            ],
-          ),
+        body: Column(
+          children: [
+            
+          ],
         )
-      ),
+             ),
     );
   }
 }
