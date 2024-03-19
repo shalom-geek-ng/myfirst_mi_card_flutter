0a9c758048556ca95140be176e71c00472d4af6e
shalom <oluwafemishalom3@gmail.com>
2024-03-19 15:19:11 +0100

    make few changes

--- lib/main.dart
+++ lib/main.dart
@@ -42,33 +42,65 @@ class MyApp extends StatelessWidget {
                  letterSpacing: 2.5,
                ),
               ),
-           Container(
+           Card(
              color: Colors.white,
              margin: EdgeInsets.symmetric(vertical: 10.0,
              horizontal: 25.0),
-             padding: EdgeInsets.symmetric(vertical: 15.0,
-             horizontal: 5.0),
-             child: Row(
-               children: [
-                 Icon(Icons.phone_in_talk,
-                 color: Colors.teal.shade900,),
-                 Text('+2348166649897', style:
-                   TextStyle(
-                     color: Colors.teal.shade900,
-                     fontSize: 20.0,
-                     fontWeight: FontWeight.bold,
-                     fontFamily: 'SourceSans3'
 
-                   ),)
+             child: Padding(
+               padding: EdgeInsets.symmetric(vertical: 15.0,
+                 horizontal: 5.0),
+               child: Row(
+                 children: [
+                   Icon(Icons.phone_in_talk,
+                   color: Colors.teal.shade900,),
+                   SizedBox(width: 10.0),
+                   Text('+2348166649897', style:
+                     TextStyle(
+                       color: Colors.teal.shade900,
+                       fontSize: 20.0,
+                       fontWeight: FontWeight.bold,
+                       fontFamily: 'SourceSans3'
 
-               ],
+                     ),)
+
+                 ],
+               ),
              ),
-           ) ],
+           ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0,
+                  horizontal: 25.0),
+
+
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0,
+                    horizontal: 5.0),
+                child: ListTile(
+                  leading: Icon(Icons.mail,
+                    color: Colors.teal.shade900,),
+                  title: Text('oluwafemishalom3@gmail.com',
+                      style:
+                      TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'SourceSans3'
+
+                      )
+
+                  )
+                  ,
+                )
+              ),
+            )
+            ],
+
           ),
+
         )
              ),
     );
   }
 }
-
-
