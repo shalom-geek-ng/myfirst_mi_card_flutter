8d7c54dcaa8acbf6502d46f802ec4369c7c28c13
shalom <oluwafemishalom3@gmail.com>
2024-03-19 16:03:33 +0100

    done with the project

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         body:
         SafeArea(
           child: Column(
-
+          mainAxisAlignment: MainAxisAlignment.center,
             children: [
 
 
@@ -42,41 +42,40 @@ class MyApp extends StatelessWidget {
                  letterSpacing: 2.5,
                ),
               ),
+              SizedBox(height: 50.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade900,
+                ),
+              ),
            Card(
              color: Colors.white,
              margin: EdgeInsets.symmetric(vertical: 10.0,
              horizontal: 25.0),
 
-             child: Padding(
-               padding: EdgeInsets.symmetric(vertical: 15.0,
-                 horizontal: 5.0),
-               child: Row(
-                 children: [
-                   Icon(Icons.phone_in_talk,
-                   color: Colors.teal.shade900,),
-                   SizedBox(width: 10.0),
-                   Text('+2348166649897', style:
-                     TextStyle(
-                       color: Colors.teal.shade900,
-                       fontSize: 20.0,
-                       fontWeight: FontWeight.bold,
-                       fontFamily: 'SourceSans3'
 
-                     ),)
+               child: ListTile(
+                leading: Icon(Icons.phone_in_talk,
+                  color: Colors.teal.shade900,),
 
-                 ],
+               title: Text('+2348166649897', style:
+               TextStyle(
+                   color: Colors.teal.shade900,
+                   fontSize: 20.0,
+                   fontWeight: FontWeight.bold,
+                   fontFamily: 'SourceSans3'
+
+               ),),
                ),
              ),
-           ),
+
             Card(
               color: Colors.white,
               margin: EdgeInsets.symmetric(vertical: 10.0,
                   horizontal: 25.0),
 
 
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0,
-                    horizontal: 5.0),
+
                 child: ListTile(
                   leading: Icon(Icons.mail,
                     color: Colors.teal.shade900,),
@@ -94,7 +93,7 @@ class MyApp extends StatelessWidget {
                   ,
                 )
               ),
-            )
+
             ],
 
           ),
@@ -104,3 +103,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
